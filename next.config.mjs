8074d4d256c9c18618c89b9abd72b4dd02806306
Anshul-Tiberius <anshul.tibrewala@gmail.com>
2025-08-17 22:49:35 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add/keep anything you had before (images, redirects, etc.)
+};
+
+export default nextConfig;
